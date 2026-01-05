35fa125247befe45b5833c688266a3ffa144aefa
Jason Preneveau <jpnamausa@gmail.com>
2026-01-05 16:50:17 -0500

    used opencode to optimize config

--- nvim-j/lua/jpp/plugins/init.lua
+++ nvim-j/lua/jpp/plugins/init.lua
@@ -7,20 +7,14 @@ return {
 	{ "nvim-lua/popup.nvim" },
 	{ "christoomey/vim-tmux-navigator" },
 	{ "bluz71/vim-nightfly-guicolors" },
-	{ "ThePrimeagen/vim-be-good" },
+
 	{ "petertriho/nvim-scrollbar" },
 	{ "kevinhwang91/nvim-hlslens" },
-	{ "gaborvecsei/usage-tracker.nvim" },
+
 	-- { "focus.nvim" },
 	{ "nvim-treesitter/playground" },
-	{
-		"codota/tabnine-nvim",
-		build = "./dl_binaries.sh",
-	},
-	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-	},
+
+
 	{
 		"ThePrimeagen/harpoon",
 		dependencies = {
